36894ab50f8f9bbc3935bcde18574fd437ffed16
Jose Ryu Leonesta <ryunamikaze777@gmail.com>
2023-06-26 16:07:28 +0700

    Init

---
+++ lua/plugins/volar.lua
@@ -0,0 +1,35 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    setup = {
+      volar = function()
+        local util = require("lspconfig.util")
+        local function get_typescript_server_path(root_dir)
+          local global_ts = "C:/Users/inter/AppData/Roaming/npm/node_modules/typescript/lib"
+          -- Alternative location if installed as root:
+          -- local global_ts = '/usr/local/lib/node_modules/typescript/lib'
+          local found_ts = ""
+          local function check_dir(path)
+            found_ts = util.path.join(path, "node_modules", "typescript", "lib")
+            if util.path.exists(found_ts) then
+              return path
+            end
+          end
+          if util.search_ancestors(root_dir, check_dir) then
+            return found_ts
+          else
+            return global_ts
+          end
+        end
+        require("lspconfig").volar.setup({
+          filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" },
+          on_new_config = function(new_config, new_root_dir)
+            new_config.init_options.typescript.tsdk = get_typescript_server_path(new_root_dir)
+          end,
+        })
+
+        return true
+      end,
+    },
+  },
+}
