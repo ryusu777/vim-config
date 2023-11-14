f7135419a28f466ed2543761f9e2f63f0ab9077f
Jose Ryu Leonesta <ryunamikaze777@gmail.com>
2023-11-14 16:39:53 +0700

    Last save

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,4 +2,5 @@ return {
   { "L3MON4D3/LuaSnip", enabled = false },
   { "rafamadriz/friendly-snippets", enabled = false },
   { "folke/flash.nvim", enabled = false },
+  { "SmiteshP/nvim-navic", enabled = false },
 }
---
+++ lua/plugins/nvim-notify.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      timeout = 2000,
+      fps = 75,
+      render = "compact",
+      stages = "static",
+    },
+  },
+}
