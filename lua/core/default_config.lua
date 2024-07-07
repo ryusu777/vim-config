b1d37cecca652e1dbffbb3362f4441f57348b2cf
Jose Ryu Leonesta <ryunamikaze777@gmail.com>
2024-07-07 13:53:33 +0700

    changed status line theme

--- lua/core/default_config.lua
+++ lua/core/default_config.lua
@@ -31,7 +31,7 @@ M.ui = {
 
   ------------------------------- nvchad_ui modules -----------------------------
   statusline = {
-    theme = "default", -- default/vscode/vscode_colored/minimal
+    theme = "vscode_colored", -- default/vscode/vscode_colored/minimal
     -- default/round/block/arrow separators work only for default statusline theme
     -- round and block will work for minimal theme only
     separator_style = "default",
@@ -41,7 +41,7 @@ M.ui = {
   -- lazyload it when there are 1+ buffers
   tabufline = {
     show_numbers = false,
-    enabled = true,
+    enabled = false,
     lazyload = true,
     overriden_modules = nil,
   },
